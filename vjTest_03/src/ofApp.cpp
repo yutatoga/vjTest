654e618ec2e3a4c3b7d2ff6cf5717001fab23873
yutatoga <yuta.toga@gmail.com>
2016-06-14 18:30:21 +0900

    added vjTest_03

---
+++ vjTest_03/src/ofApp.cpp
@@ -0,0 +1,218 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofEnableSmoothing();
+    ofBackground(0);
+    
+    // voronoi
+    voronoi.setBounds(ofGetWindowRect());
+    
+    // add random 5 points
+    for (int i = 0; i < 5; i++) {
+        ofPoint instantPoint(ofRandom(0, ofGetWidth()), ofRandom(0, ofGetHeight()));
+        
+        // delaunay
+        triangulation.addPoint(instantPoint);
+        
+        // voronoi
+        voronoi.addPoint(instantPoint);
+    }
+    
+    // delaunay
+    triangulation.triangulate();
+    
+    // voronoi
+    voronoi.generate();
+    
+    // sound
+    player.load("beat.mp3");
+    player.play();
+    
+    // webcam
+    videoGrabber.setDeviceID(0);
+    videoGrabber.setVerbose(true);
+    videoGrabber.setDesiredFrameRate(60);
+    videoGrabber.initGrabber(GRABBER_WIDTH, GERBBER_HEIGHT);
+    
+    // gui
+    panel.setup();
+    panel.add(showDelaunayBackground.set("showDelaunayBackground", true));
+    panel.add(showDelaunayWireframe.set("showDelaunayWireframe", true));
+    panel.add(showDelaunayVertices.set("showDelaunayVertices", true));
+    panel.add(showDelaunayCenter.set("showDelaunayCenter", true));
+    panel.add(showVoronoi.set("showVoronoi", true));    
+    
+    showGui = true;
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    // update webcam
+    videoGrabber.update();
+    
+    // update points with sound
+    float volume = ofSoundGetSpectrum(1)[0]*10;
+    if (volume > 0.5) {
+        // ADD
+        
+        // voronoi
+        voronoi.getCells().clear();
+        ofPoint instantPoint(ofRandom(0, ofGetWidth()), ofRandom(0, ofGetHeight()));
+        voronoi.addPoint(instantPoint);
+        voronoi.generate();
+        
+        // delaunay
+        triangulation.addPoint(instantPoint);
+        triangulation.triangulate();
+    } else if ((int) voronoi.getCells().size() > 3) {
+        // REMOVE
+        
+        // voronoi
+        voronoi.getCells().clear();
+        voronoi.getPoints().pop_back();
+        voronoi.generate();
+        
+        // delaunay
+        triangulation.removePointAtIndex(triangulation.getNumPoints()-1);
+        triangulation.triangulate();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    // draw the background of triangles
+    if (showDelaunayBackground) {
+        ofSetColor(0, 0, 127);
+        ofFill();
+        triangulation.draw();
+        ofSetColor(255);
+    }
+    
+    // draw wire frame of triangles
+    if (showDelaunayWireframe) {
+        ofNoFill();
+        ofSetColor(255);
+        triangulation.draw();
+        ofFill();
+    }
+    
+    // draw vertices of triangles
+    if (showDelaunayVertices) {
+        ofFill();
+        glPointSize(10.0);
+        ofSetColor(255, 0, 0, 255);
+        ofMesh mesh = triangulation.triangleMesh;
+        mesh.setMode(OF_PRIMITIVE_POINTS);
+        mesh.draw();
+        glPointSize(1.0);
+        ofSetColor(255);
+    }
+    
+    // draw centers of the mesh
+    if (showDelaunayCenter) {
+        ofSetColor(0, 255, 0, 255);
+        for (int i = 0; i < triangulation.getNumTriangles(); i++) {
+            int index1 = triangulation.triangleMesh.getIndex(i*3);
+            int index2 = triangulation.triangleMesh.getIndex(i*3+1);
+            int index3 = triangulation.triangleMesh.getIndex(i*3+2);
+            
+            ofVec3f vertex1 = triangulation.triangleMesh.getVertex(index1);
+            ofVec3f vertex2 = triangulation.triangleMesh.getVertex(index2);
+            ofVec3f vertex3 = triangulation.triangleMesh.getVertex(index3);
+            
+            ofVec3f center = (vertex1+vertex2+vertex3)/3.0;
+            
+            ofDrawCircle(center, 5);
+        }
+        ofSetColor(255);
+    }
+    
+    // vornoi
+    if (showVoronoi) {
+        voronoi.draw();
+        ofSetColor(255);
+    };
+    
+    // webcam
+    image.setFromPixels(videoGrabber.getPixels());
+    image.mirror(false, true);
+    image.draw(ofGetWidth()-GRABBER_WIDTH, 0, GRABBER_WIDTH, GERBBER_HEIGHT);
+    
+    // gui
+    if (showGui) panel.draw();
+    
+    // debug
+    ofDrawBitmapString("'r' to reset", ofPoint(10,20));
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    switch (key) {
+        case 'r':
+            triangulation.reset();
+            break;
+        case 'h':
+            showGui = !showGui;
+            break;
+        default:
+            break;
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    // delaunay
+    triangulation.addPoint(ofPoint(x, y));
+    triangulation.triangulate();
+    
+    // voronoi
+    voronoi.getCells().clear();
+    voronoi.addPoint(ofPoint(x, y));
+    voronoi.generate();
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
